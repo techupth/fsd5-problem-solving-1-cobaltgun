104e7d884f65effc1b2e905e0bf773f05b1254dc
Gun <cobaltgun2324@gmail.com>
2023-12-20 18:10:41 +0700

    feat: complete problem solving

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,14 @@
 
 const isPalindrome = function (s) {
   //Start Coding here
+  let cleanString = "";
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i].toLowerCase();
+    if ((char >= "a" && char <= "z") || (char >= "0" && char <= "9")) {
+      cleanString = cleanString + char;
+    }
+  }
+  return cleanString.split("").reverse().join("") === cleanString;
 };
 
 const result1 = isPalindrome("A man, a plan, a canal: Panama");
